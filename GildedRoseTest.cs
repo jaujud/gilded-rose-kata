b889ca5257f67ddb803858d7c64304a5969ada14
Jaunius <j.judackas@gmail.com>
2019-10-27 20:50:51 +0200

    Update README.md. Implement tests for GildedRose class.

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 
 namespace csharp
@@ -7,12 +8,202 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void ShouldDecreseQualityForRegularItems()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> 
+            { 
+                new Item { Name = "Arrow", SellIn = 0, Quality = 0 },
+                new Item { Name = "Apple", SellIn = 1, Quality = 10 },
+                new Item { Name = "Sword", SellIn = 0, Quality = 10 },
+                new Item { Name = "Backpack", SellIn = -1, Quality = 10 }
+            };
+
+            IList<Item> ItemsExpected = new List<Item>
+            {
+                new Item { Name = "Arrow", SellIn = -1, Quality = 0 },
+                new Item { Name = "Apple", SellIn = 0, Quality = 9 },
+                new Item { Name = "Sword", SellIn = -1, Quality = 8 },
+                new Item { Name = "Backpack", SellIn = -2, Quality = 8 }
+            };
+
             GildedRose app = new GildedRose(Items);
+
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+
+            Item item;
+            Item itemExpected;
+
+            for (int i = 0; i < Items.Count; i++)
+            {
+                item = Items[i];
+                itemExpected = ItemsExpected[i];
+
+                Assert.AreEqual(itemExpected.Name, item.Name, $"Item: {i}, Error: Expected Name '{itemExpected.Name}' but found '{item.Name}'");
+                Assert.AreEqual(itemExpected.SellIn, item.SellIn, $"Item: {i}, Error: Expected SellIn '{itemExpected.SellIn}' but found '{item.SellIn}'");
+                Assert.AreEqual(itemExpected.Quality, item.Quality, $"Item: {i}, Error: Expected Quality '{itemExpected.Quality}' but found '{item.Quality}'");
+            }
+        }
+
+        [Test]
+        public void ShouldDecreseQualityForConjuredItems()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item { Name = "Conjured Arrow", SellIn = 0, Quality = 0 },
+                new Item { Name = "Conjured Apple", SellIn = 1, Quality = 10 },
+                new Item { Name = "+5 Conjured Sword", SellIn = 0, Quality = 10 },
+                new Item { Name = "Conjured Backpack", SellIn = -1, Quality = 10 }
+            };
+
+            IList<Item> ItemsExpected = new List<Item>
+            {
+                new Item { Name = "Conjured Arrow", SellIn = -1, Quality = 0 },
+                new Item { Name = "Conjured Apple", SellIn = 0, Quality = 8 },
+                new Item { Name = "+5 Conjured Sword", SellIn = -1, Quality = 6 },
+                new Item { Name = "Conjured Backpack", SellIn = -2, Quality = 6 }
+            };
+
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Item item;
+            Item itemExpected;
+
+            for (int i = 0; i < Items.Count; i++)
+            {
+                item = Items[i];
+                itemExpected = ItemsExpected[i];
+
+                Assert.AreEqual(itemExpected.Name, item.Name, $"Item: {i}, Error: Expected Name '{itemExpected.Name}' but found '{item.Name}'");
+                Assert.AreEqual(itemExpected.SellIn, item.SellIn, $"Item: {i}, Error: Expected SellIn '{itemExpected.SellIn}' but found '{item.SellIn}'");
+                Assert.AreEqual(itemExpected.Quality, item.Quality, $"Item: {i}, Error: Expected Quality '{itemExpected.Quality}' but found '{item.Quality}'");
+            }
+        }
+
+        [Test]
+        public void ShouldIncreaseQualityForAgedBrie()
+        {
+            var itemName = "Aged Brie";
+            IList<Item> Items = new List<Item>
+            {
+                new Item { Name = itemName, SellIn = 1, Quality = 10 },
+                new Item { Name = itemName, SellIn = 0, Quality = 10 },
+                new Item { Name = itemName, SellIn = -1, Quality = 10 },
+                new Item { Name = itemName, SellIn = -1, Quality = 50 }
+            };
+            IList<Item> ItemsExpected = new List<Item>
+            {
+                new Item { Name = itemName, SellIn = 0, Quality = 11 },
+                new Item { Name = itemName, SellIn = -1, Quality = 12 },
+                new Item { Name = itemName, SellIn = -2, Quality = 12 },
+                new Item { Name = itemName, SellIn = -2, Quality = 50 }
+            };
+
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Item item;
+            Item itemExpected;
+
+            for (int i = 0; i < Items.Count; i++)
+            {
+                item = Items[i];
+                itemExpected = ItemsExpected[i];
+
+                Console.WriteLine($"item: {item.Name}, {item.SellIn}, {item.Quality}");
+                Console.WriteLine($"itemExpected: {item.Name}, {item.SellIn}, {item.Quality}");
+                
+
+                Assert.AreEqual(itemExpected.Name, item.Name, $"Item: {i}, Error: Expected Name '{itemExpected.Name}' but found '{item.Name}'");
+                Assert.AreEqual(itemExpected.SellIn, item.SellIn, $"Item: {i}, Error: Expected SellIn '{itemExpected.SellIn}' but found '{item.SellIn}'");
+                Assert.AreEqual(itemExpected.Quality, item.Quality, $"Item: {i}, Error: Expected Quality '{itemExpected.Quality}' but found '{item.Quality}'");
+            }
+        }
+
+        [Test]
+        public void ShouldIncreaseQualityForBackstagePasses()
+        {
+            var itemName = "Backstage passes to a TAFKAL80ETC concert";
+            IList<Item> Items = new List<Item>
+            {
+                new Item { Name = itemName, SellIn = 11, Quality = 5 },
+                new Item { Name = itemName, SellIn = 10, Quality = 5 },
+                new Item { Name = itemName, SellIn = 9, Quality = 49 },
+                new Item { Name = itemName, SellIn = 5, Quality = 5 },
+                new Item { Name = itemName, SellIn = 4, Quality = 49 },
+                new Item { Name = itemName, SellIn = 0, Quality = 5 }
+            };
+
+            IList<Item> ItemsExpected = new List<Item>
+            {
+                new Item { Name = itemName, SellIn = 10, Quality = 6 },
+                new Item { Name = itemName, SellIn = 9, Quality = 7 },
+                new Item { Name = itemName, SellIn = 8, Quality = 50 },
+                new Item { Name = itemName, SellIn = 4, Quality = 8 },
+                new Item { Name = itemName, SellIn = 3, Quality = 50 },
+                new Item { Name = itemName, SellIn = -1, Quality = 0 }
+            };
+
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Item item;
+            Item itemExpected;
+
+            for (int i = 0; i < Items.Count; i++)
+            {
+                item = Items[i];
+                itemExpected = ItemsExpected[i];
+
+                Console.WriteLine($"item: {item.Name}, {item.SellIn}, {item.Quality}");
+                Console.WriteLine($"itemExpected: {item.Name}, {item.SellIn}, {item.Quality}");
+
+                Assert.AreEqual(itemExpected.Name, item.Name, $"Item: {i}, Error: Expected Name '{itemExpected.Name}' but found '{item.Name}'");
+                Assert.AreEqual(itemExpected.SellIn, item.SellIn, $"Item: {i}, Error: Expected SellIn '{itemExpected.SellIn}' but found '{item.SellIn}'");
+                Assert.AreEqual(itemExpected.Quality, item.Quality, $"Item: {i}, Error: Expected Quality '{itemExpected.Quality}' but found '{item.Quality}'");
+            }
+        }
+
+        [Test]
+        public void ShouldNotUpdateQualityForSulfuras()
+        {
+            var itemName = "Sulfuras, Hand of Ragnaros";
+            IList<Item> Items = new List<Item>
+            {
+                new Item { Name = itemName, SellIn = 1, Quality = 80 },
+                new Item { Name = itemName, SellIn = 0, Quality = 80 },
+                new Item { Name = itemName, SellIn = -1, Quality = 80 }
+            };
+
+            IList<Item> ItemsExpected = new List<Item>
+            {
+                new Item { Name = itemName, SellIn = 1, Quality = 80 },
+                new Item { Name = itemName, SellIn = 0, Quality = 80 },
+                new Item { Name = itemName, SellIn = -1, Quality = 80 }
+            };
+
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Item item;
+            Item itemExpected;
+
+            for (int i = 0; i < Items.Count; i++)
+            {
+                item = Items[i];
+                itemExpected = ItemsExpected[i];
+
+                Console.WriteLine($"item: {item.Name}, {item.SellIn}, {item.Quality}");
+                Console.WriteLine($"itemExpected: {item.Name}, {item.SellIn}, {item.Quality}");
+
+                Assert.AreEqual(itemExpected.Name, item.Name, $"Item: {i}, Error: Expected Name '{itemExpected.Name}' but found '{item.Name}'");
+                Assert.AreEqual(itemExpected.SellIn, item.SellIn, $"Item: {i}, Error: Expected SellIn '{itemExpected.SellIn}' but found '{item.SellIn}'");
+                Assert.AreEqual(itemExpected.Quality, item.Quality, $"Item: {i}, Error: Expected Quality '{itemExpected.Quality}' but found '{item.Quality}'");
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # gilded-rose-kata
 First attempt at Gilded Rose kata in C#
+
+Full requirements at the time being can be found in GildedRoseRequirements.txt
+
+
+#NOTES
+
+There are some uncertainties in the requirements specification.
+
+1. Requirment: "Once the sell by date has passed, Quality **degrades** twice as fast"
+
+One would think that per this requirement only the degradation should be multiplied. However in the initial implementation the rate is also increased for special items such as Aged Brie which does not degrade but gain quality by each iteration.
+
+2. Requirement for new feature: "'Conjured' items degrade in Quality twice as fast as normal items"
+
+It is not stated if only regular items can be "Conjured" or there can be special "Conjured" items like "Conjured Adged Brie". As it is impossible to clarify this, it is asumed the only regular items can be "Cojured".
