ec5e06f5d9e7d90c6cff540eaaae82ac37f8a6b7
Jaunius <j.judackas@gmail.com>
2019-10-28 12:52:51 +0200

    Rename project and solution

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -1,8 +1,7 @@
 ﻿using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 
-namespace csharp
+namespace GildedRose
 {
     [TestFixture]
     public class GildedRoseTest
@@ -10,123 +9,78 @@ namespace csharp
         [Test]
         public void ShouldDecreseQualityForRegularItems()
         {
-            IList<Item> Items = new List<Item> 
+            var items = new List<Item> 
             { 
                 new Item { Name = "Arrow", SellIn = 0, Quality = 0 },
                 new Item { Name = "Apple", SellIn = 1, Quality = 10 },
                 new Item { Name = "Sword", SellIn = 0, Quality = 10 },
                 new Item { Name = "Backpack", SellIn = -1, Quality = 10 }
             };
-
-            IList<Item> ItemsExpected = new List<Item>
+            var expectedItems = new List<Item>
             {
                 new Item { Name = "Arrow", SellIn = -1, Quality = 0 },
                 new Item { Name = "Apple", SellIn = 0, Quality = 9 },
                 new Item { Name = "Sword", SellIn = -1, Quality = 8 },
                 new Item { Name = "Backpack", SellIn = -2, Quality = 8 }
             };
-
-            GildedRose app = new GildedRose(Items);
+            GildedRose app = new GildedRose(items);
 
             app.UpdateQuality();
-
-            Item item;
-            Item itemExpected;
-
-            for (int i = 0; i < Items.Count; i++)
-            {
-                item = Items[i];
-                itemExpected = ItemsExpected[i];
-
-                Assert.AreEqual(itemExpected.Name, item.Name, $"Item: {i}, Error: Expected Name '{itemExpected.Name}' but found '{item.Name}'");
-                Assert.AreEqual(itemExpected.SellIn, item.SellIn, $"Item: {i}, Error: Expected SellIn '{itemExpected.SellIn}' but found '{item.SellIn}'");
-                Assert.AreEqual(itemExpected.Quality, item.Quality, $"Item: {i}, Error: Expected Quality '{itemExpected.Quality}' but found '{item.Quality}'");
-            }
+            CompareItems(items, expectedItems);
         }
 
         [Test]
         public void ShouldDecreseQualityForConjuredItems()
         {
-            IList<Item> Items = new List<Item>
+            var items = new List<Item>
             {
                 new Item { Name = "Conjured Arrow", SellIn = 0, Quality = 0 },
                 new Item { Name = "Conjured Apple", SellIn = 1, Quality = 10 },
                 new Item { Name = "+5 Conjured Sword", SellIn = 0, Quality = 10 },
                 new Item { Name = "Conjured Backpack", SellIn = -1, Quality = 10 }
             };
-
-            IList<Item> ItemsExpected = new List<Item>
+            var expectedItems = new List<Item>
             {
                 new Item { Name = "Conjured Arrow", SellIn = -1, Quality = 0 },
                 new Item { Name = "Conjured Apple", SellIn = 0, Quality = 8 },
                 new Item { Name = "+5 Conjured Sword", SellIn = -1, Quality = 6 },
                 new Item { Name = "Conjured Backpack", SellIn = -2, Quality = 6 }
             };
-
-            GildedRose app = new GildedRose(Items);
+            GildedRose app = new GildedRose(items);
 
             app.UpdateQuality();
-
-            Item item;
-            Item itemExpected;
-
-            for (int i = 0; i < Items.Count; i++)
-            {
-                item = Items[i];
-                itemExpected = ItemsExpected[i];
-
-                Assert.AreEqual(itemExpected.Name, item.Name, $"Item: {i}, Error: Expected Name '{itemExpected.Name}' but found '{item.Name}'");
-                Assert.AreEqual(itemExpected.SellIn, item.SellIn, $"Item: {i}, Error: Expected SellIn '{itemExpected.SellIn}' but found '{item.SellIn}'");
-                Assert.AreEqual(itemExpected.Quality, item.Quality, $"Item: {i}, Error: Expected Quality '{itemExpected.Quality}' but found '{item.Quality}'");
-            }
+            CompareItems(items, expectedItems);
         }
 
         [Test]
         public void ShouldIncreaseQualityForAgedBrie()
         {
             var itemName = "Aged Brie";
-            IList<Item> Items = new List<Item>
+            var items = new List<Item>
             {
                 new Item { Name = itemName, SellIn = 1, Quality = 10 },
                 new Item { Name = itemName, SellIn = 0, Quality = 10 },
                 new Item { Name = itemName, SellIn = -1, Quality = 10 },
                 new Item { Name = itemName, SellIn = -1, Quality = 49 }
             };
-            IList<Item> ItemsExpected = new List<Item>
+            var expectedItems = new List<Item>
             {
                 new Item { Name = itemName, SellIn = 0, Quality = 11 },
                 new Item { Name = itemName, SellIn = -1, Quality = 12 },
                 new Item { Name = itemName, SellIn = -2, Quality = 12 },
                 new Item { Name = itemName, SellIn = -2, Quality = 50 }
             };
-
-            GildedRose app = new GildedRose(Items);
+            GildedRose app = new GildedRose(items);
 
             app.UpdateQuality();
-
-            Item item;
-            Item itemExpected;
-
-            for (int i = 0; i < Items.Count; i++)
-            {
-                item = Items[i];
-                itemExpected = ItemsExpected[i];
-
-                Console.WriteLine($"item: {item.Name}, {item.SellIn}, {item.Quality}");
-                Console.WriteLine($"itemExpected: {item.Name}, {item.SellIn}, {item.Quality}");
-                
-
-                Assert.AreEqual(itemExpected.Name, item.Name, $"Item: {i}, Error: Expected Name '{itemExpected.Name}' but found '{item.Name}'");
-                Assert.AreEqual(itemExpected.SellIn, item.SellIn, $"Item: {i}, Error: Expected SellIn '{itemExpected.SellIn}' but found '{item.SellIn}'");
-                Assert.AreEqual(itemExpected.Quality, item.Quality, $"Item: {i}, Error: Expected Quality '{itemExpected.Quality}' but found '{item.Quality}'");
-            }
+            CompareItems(items, expectedItems);
         }
 
         [Test]
         public void ShouldIncreaseQualityForBackstagePasses()
         {
             var itemName = "Backstage passes to a TAFKAL80ETC concert";
-            IList<Item> Items = new List<Item>
+            var items = new List<Item>
             {
                 new Item { Name = itemName, SellIn = 11, Quality = 5 },
                 new Item { Name = itemName, SellIn = 10, Quality = 5 },
@@ -135,8 +89,7 @@ namespace csharp
                 new Item { Name = itemName, SellIn = 4, Quality = 49 },
                 new Item { Name = itemName, SellIn = 0, Quality = 5 }
             };
-
-            IList<Item> ItemsExpected = new List<Item>
+            var expectedItems = new List<Item>
             {
                 new Item { Name = itemName, SellIn = 10, Quality = 6 },
                 new Item { Name = itemName, SellIn = 9, Quality = 7 },
@@ -145,64 +98,48 @@ namespace csharp
                 new Item { Name = itemName, SellIn = 3, Quality = 50 },
                 new Item { Name = itemName, SellIn = -1, Quality = 0 }
             };
-
-            GildedRose app = new GildedRose(Items);
+            GildedRose app = new GildedRose(items);
 
             app.UpdateQuality();
-
-            Item item;
-            Item itemExpected;
-
-            for (int i = 0; i < Items.Count; i++)
-            {
-                item = Items[i];
-                itemExpected = ItemsExpected[i];
-
-                Console.WriteLine($"item: {item.Name}, {item.SellIn}, {item.Quality}");
-                Console.WriteLine($"itemExpected: {item.Name}, {item.SellIn}, {item.Quality}");
-
-                Assert.AreEqual(itemExpected.Name, item.Name, $"Item: {i}, Error: Expected Name '{itemExpected.Name}' but found '{item.Name}'");
-                Assert.AreEqual(itemExpected.SellIn, item.SellIn, $"Item: {i}, Error: Expected SellIn '{itemExpected.SellIn}' but found '{item.SellIn}'");
-                Assert.AreEqual(itemExpected.Quality, item.Quality, $"Item: {i}, Error: Expected Quality '{itemExpected.Quality}' but found '{item.Quality}'");
-            }
+            CompareItems(items, expectedItems);
         }
 
         [Test]
         public void ShouldNotUpdateQualityForSulfuras()
         {
             var itemName = "Sulfuras, Hand of Ragnaros";
-            IList<Item> Items = new List<Item>
+            var items = new List<Item>
             {
                 new Item { Name = itemName, SellIn = 1, Quality = 80 },
                 new Item { Name = itemName, SellIn = 0, Quality = 80 },
                 new Item { Name = itemName, SellIn = -1, Quality = 80 }
             };
-
-            IList<Item> ItemsExpected = new List<Item>
+            var expectedItems = new List<Item>
             {
                 new Item { Name = itemName, SellIn = 1, Quality = 80 },
                 new Item { Name = itemName, SellIn = 0, Quality = 80 },
                 new Item { Name = itemName, SellIn = -1, Quality = 80 }
             };
-
-            GildedRose app = new GildedRose(Items);
+            GildedRose app = new GildedRose(items);
 
             app.UpdateQuality();
+            CompareItems(items, expectedItems);
+        }
 
+        private void CompareItems(IList<Item> items, IList<Item> expectedItems)
+        {
             Item item;
-            Item itemExpected;
+            Item expectedItem;
+            var messageTemplate = "Item#: {0}, Error: {1} does not match the expected value";
 
-            for (int i = 0; i < Items.Count; i++)
+            for (var i = 0; i < items.Count; i++)
             {
-                item = Items[i];
-                itemExpected = ItemsExpected[i];
+                item = items[i];
+                expectedItem = expectedItems[i];
 
-                Console.WriteLine($"item: {item.Name}, {item.SellIn}, {item.Quality}");
-                Console.WriteLine($"itemExpected: {item.Name}, {item.SellIn}, {item.Quality}");
-
-                Assert.AreEqual(itemExpected.Name, item.Name, $"Item: {i}, Error: Expected Name '{itemExpected.Name}' but found '{item.Name}'");
-                Assert.AreEqual(itemExpected.SellIn, item.SellIn, $"Item: {i}, Error: Expected SellIn '{itemExpected.SellIn}' but found '{item.SellIn}'");
-                Assert.AreEqual(itemExpected.Quality, item.Quality, $"Item: {i}, Error: Expected Quality '{itemExpected.Quality}' but found '{item.Quality}'");
+                Assert.AreEqual(expectedItem.Name, item.Name, string.Format(messageTemplate, i, "Name"));
+                Assert.AreEqual(expectedItem.SellIn, item.SellIn, string.Format(messageTemplate, i, "SellIn"));
+                Assert.AreEqual(expectedItem.Quality, item.Quality, string.Format(messageTemplate, i, "Quality"));
             }
         }
     }
